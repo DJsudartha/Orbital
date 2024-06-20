7a442b2d0ee422c1c88e246eeb2eb6f555c1140d
kea-S <keaharvan1@gmail.com>
2024-06-20 18:47:40 +0800

    prepping file structure to add Metronome feature in

---
+++ src/Features/Metronome/frontend/src/components/Metronome.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Button from 'react-bootstrap/esm/Button'
+import FormRange from 'react-bootstrap/esm/FormRange'
+import { click1, click2 } from '../assets/sounds'
+import Timer from '../assets/timer'
+
+const Metronome = ({ songData, handleSongDataChange }) => {
+    const { Tempo, TimeSignature } = songData;
+
+    /* NOTE: TimeSignature is a STRING, but we need it to be an INTEGER
+    for calculation purposes, thus */
+    const numericTimeSignature = [parseInt(TimeSignature[0]),
+    parseInt(TimeSignature[2])];
+
+    // metronome on/off
+    const [playOn, setPlayOn] = useState(false);
+
+    // keeps track of the time signature
+    let beatCountTracker = 1;
+
+    const keepTime = () => {
+        const beatCount = beatCountTracker;
+
+        if (beatCount % numericTimeSignature[0] === 0) {
+            click2.play();
+        } else {
+            click1.play();
+        }
+
+        beatCountTracker = (beatCount + 1) % numericTimeSignature[0];
+    }
+
+    // update state to use setInterval
+    const [timerID, setTimerID] = useState(0);
+    // FIX: if switch page metronome still plays
+    useEffect(() => { // can try make this more accurate using the timer function
+        if (playOn) {
+            click2.play(); // play instantly
+            const intervalID = setInterval(keepTime, 60000 / Tempo);
+            setTimerID(intervalID);
+        } else {
+            clearInterval(timerID);
+            setTimerID(0);
+        }
+
+        return () => clearInterval(timerID);
+    }, [playOn]);
+
+    return (
+        <div className='container'>
+            <div className='row h-25 d-flex align-items-center'>
+                <div className='d-flex justify-content-center fs-1'>
+                    BPM: {Tempo}
+                </div>
+            </div>
+            <div className='row h-25 d-flex align-items-center justify-content-center'>
+                <div className='col-8 d-flex justify-content-center'>
+                    <FormRange min={60} max={200}
+                        value={Tempo}
+                        onChange={(event) => {
+                            handleSongDataChange("tempoRange",
+                                event.target.value);
+                        }} />
+                </div>
+            </div>
+            <div className='row h-25 d-flex align-items-center'>
+                <div className='col-6 d-flex justify-content-end'>
+                    <DropdownButton id="dropdown-basic-button" variant='info' title="Time Signature">
+                        <Dropdown.Item onClick={() =>
+                            handleSongDataChange("timeSignatureDropDown", "4/4")}>
+                            4/4</Dropdown.Item>
+                        <Dropdown.Item onClick={() =>
+                            handleSongDataChange("timeSignatureDropDown", "3/4")}>
+                            3/4</Dropdown.Item>
+                        <Dropdown.Item onClick={() =>
+                            handleSongDataChange("timeSignatureDropDown", "2/4")}>
+                            2/4</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+                <div className='col-6 d-flex justify-content-start fs-5'>
+                    {numericTimeSignature[0]}/{numericTimeSignature[1]}
+                </div>
+            </div>
+            <div className='row h-25 d-flex align-items-center'>
+                <div className='col d-flex justify-content-center'>
+                    <Button variant='info' onClick={() => setPlayOn(!playOn)}>
+                        {playOn ? <div>Stop</div> : <div>Play</div>}
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Metronome
