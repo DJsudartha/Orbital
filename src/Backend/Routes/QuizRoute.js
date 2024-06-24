0554d4258b3c385ca866a88117afa6ccfba2a7c2
kea-S <keaharvan1@gmail.com>
2024-06-24 18:06:53 +0800

    got CRUD  of backend to work - unit tested

--- src/Backend/Routes/QuizRoute.js
+++ src/Backend/Routes/QuizRoute.js
@@ -33,4 +33,88 @@ QuizJourney.post("/", async (request, response) => {
 }
 );
 
+// Read All / get Journey
+QuizJourney.get("/", async (request, response) => {
+    try {
+        const journey = await QuizJourneyModel.find({});
+
+        return response.status(200).json(journey); // problems w/ wrapper'?
+    } catch (error) {
+        console.log(error.Message);
+        return response.status(500).send({ message: error.message });
+    }
+}
+);
+
+// Read Single
+QuizJourney.get("/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const quizFound = await QuizJourneyModel.findById(id);
+
+        if (!quizFound) {
+            return response
+                .status(404)
+                .send({ message: "Quiz not found" });
+
+        }
+
+        // <-> json and send for diff purposes
+        return response.status(200).json({quiz: quizFound}); 
+    } catch (error) {
+        console.log(error.Message);
+        return response.status(500).send({ message: error.message });
+    }
+}
+);
+
+// Update
+QuizJourney.put("/:id", async (request, response) => {
+    try {
+        if (!(request.body.QuizID && request.body.Correct &&
+            request.body.Question && request.body.Answers)) {
+            return response
+                .status(400)
+                .send({ message: "Incomplete data" });
+        }
+
+        const { id } = request.params;
+        const quizFound = await QuizJourneyModel.findByIdAndUpdate(id, request.body);
+
+        if (!quizFound) {
+            return response
+                .status(404)
+                .send({ message: "Quiz not found" });
+
+        }
+
+        return response.status(200).json(quizFound);
+    } catch (error) {
+        console.log(error.Message);
+        return response.status(500).send({ message: error.message });
+    }
+}
+);
+
+// Delete
+QuizJourney.delete("/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const quizFound = await QuizJourneyModel.findByIdAndDelete(id);
+
+        if (!quizFound) {
+            return response
+                .status(404)
+                .send({ message: "Quiz not found" });
+
+        }
+
+        return response.status(200).send({deleted: quizFound});
+    } catch (error) {
+        console.log(error.Message);
+        return response.status(500).send({ message: error.message });
+    }
+}
+);
+
 export default QuizJourney;
