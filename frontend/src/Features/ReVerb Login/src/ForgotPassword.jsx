185e1b3f1df52d1c4e0583373d5f55fde12074f7
DJsudartha <djsudartha@gmail.com>
2024-06-28 21:46:22 +0800

    Frontend of login page into main

---
+++ frontend/src/Features/ReVerb Login/src/ForgotPassword.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+
+
+function ForgotPassword() {
+    const [email, setEmail] = useState("")
+    const navigate = useNavigate()
+
+    axios.defaults.withCredentials = true
+    const handleSubmit = (x) => {
+        x.preventDefault()
+        axios.post('http://localhost:3001/forgot-password', {email})
+        .then(y => {
+            console.log(y)
+            if(y.data === "Success") {
+                navigate('/login')
+            }
+        })
+        .catch(err => console.log(err))
+    }
+    return (
+        <div className = "d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className = "bg-white p-3 rounded w-25">
+                <h3>Forgot Password </h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Account Email</strong>
+                        </label>
+                        <input
+                        type="email"
+                        placeholder="Enter Email"
+                        autoComplete="off"
+                        name="email"
+                        className="form-control rounded-0"
+                        onChange={(x) => setEmail(x.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Send Email
+                    </button>
+                    </form>
+            </div>
+        </div>
+    );
+
+}
+
+
+export default ForgotPassword;
