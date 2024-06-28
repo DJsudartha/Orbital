092f7bb4ea5806fc1ebb3d7aba3f61a0a27c0f60
kea-S <keaharvan1@gmail.com>
2024-06-28 14:12:29 +0800

    readying main for initial deployment

---
+++ Backend/api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { port } from "../Port.js";
+import mongoose from "mongoose";
+import { MetronomeDBURL } from "../Port.js";
+import Metronome from "../Routes/MetronomeRoute.js";
+import QuizJourney from "../Routes/QuizRoute.js";
+import cors from 'cors';
+
+const homePlaceHolder = express();
+
+homePlaceHolder.use(express.json());
+
+homePlaceHolder.use(cors());
+
+homePlaceHolder.get("/", (request, response) => {
+        console.log(request);
+        return response.json("hello");
+    }
+);
+
+homePlaceHolder.use("/metronome", Metronome);
+
+homePlaceHolder.use("/musicJourney", QuizJourney);
+// need to refactor soon
+
+mongoose
+    .connect(MetronomeDBURL)
+    .then(() => {
+        console.log("connected to DB, attempting to connect to backend server");
+        homePlaceHolder
+            .listen(port, () => console.log("connected to backend server")); 
+    })
+    .catch((error) => {
+        console.log(error);
+    }
+);
