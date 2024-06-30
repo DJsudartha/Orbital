ed6a65306b5633b1da31e66787933f342a83a5ab
DJsudartha <djsudartha@gmail.com>
2024-06-30 20:22:42 +0800

    Update Port.js

--- Backend/Port.js
+++ Backend/Port.js
@@ -1,4 +1,2 @@
 export const port = 1234; // change later on when we finish home page
 export const MetronomeDBURL = "mongodb+srv://ReVerb:Orbital24@reverb-metronome.nzdpgjb.mongodb.net/Metronome?retryWrites=true&w=majority&appName=Reverb-Metronome"
-export const MONGODB_LOGIN = "mongodb+srv://ReVerb:IdkWtfToDo@reverb.fi5it34.mongodb.net/Login?retryWrites=true&w=majority&appName=ReVerb"
-
