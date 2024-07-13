ebdcb77901befc706aab2f641f4547a70d834764
kea-S <keaharvan1@gmail.com>
2024-07-13 17:47:57 +0800

    ideating how the hearts system could work

--- frontend/src/components/MusicStarterJourney/Hearts.jsx
+++ frontend/src/components/MusicStarterJourney/Hearts.jsx
@@ -6,6 +6,11 @@ import axios from 'axios'
 const Hearts = (props, ref) => {
     const [hearts, setHearts] = useState(3); // axios.get in journey, pass it into testInterface, pass it here, testInterface calls the function here cus idw to clutter
 
+    /** const lastLoggedTime = props.whatever // passed down from journey 
+     * const hearts = props.hearts // passed down from journey
+    */
+
+
     /** on umnount, use useEffect to axios.put() the new heart count, make it a 
      * "feature" that hearts will be shared throughout journeys so u can't spam
      * ways to earn bacck hearts? wait x minutes to get 1 heart, make it quick 
@@ -17,6 +22,8 @@ const Hearts = (props, ref) => {
      * only POST here, do not GET. Get in testInterval only
     */
 
+    // need to pass in hearts as a prop from journey to testInterval aswell 
+
     useImperativeHandle(ref, () => {
         return {
             crementHearts: (crement) => setHearts(previous => previous + crement)
@@ -24,12 +31,38 @@ const Hearts = (props, ref) => {
     }, []);
 
 
-    // on unmount, update the db
+    /** I want to update the hearts whenever
+     *      1. a user gets a question wrong, -1
+     *          locally, you should update the hearts count as well as the database, 
+     *          but u don't actually get the hearts remaining from the DB here
+     *          so not "one true state"...  should be fine? what are the edge cases
+     *          here?, the post fails... but ur checking if the hearts = 0 locally aswell,
+     *          and if it does reach zero just force the db to update to zero to match the state
+     *          
+     *      2. x minutes have passed clientside +1/x mins
+     *       (this requires user to be on the site when this component is rendered, wonky)
+     *      3. what is eaier for now: component is unmounted/mounted, in the database
+     *          if current time has surpassed however many minutes from the last time put on the database
+     *          - this requires logging the time when the user logs in to a journey/ logs out of a journey
+     *      4. if the user has no more hearts remaining locally, kick that user out to journey and then
+     *          update the thing (should be done in test Interval or here?, I think heres better)
+     */
+
+    // maybe I have to seperate useEffects, one to update the DB and the other
+    // to update hearts based on time..., will there be crossbreeding there?
     useEffect(() => {
+        /** if (hearts === 0) {
+         *  navigate to journey (hopefully this also unmounts and runs cleanup function)
+         * }
+         * if (local time >= some interval of time in DB passed down from journey) {
+         *      setHearts(prev => prev + 1) // I cant do this because then it would be infinite
+         *      axios.put(setHearts)
+         * }
+        */
         return () => {
             console.log("DB updated!");
         }
-    }, []);
+    }, [hearts]);
 
     return (
         <Container ref={ref}>
