22fc2a0a29ea827ca76d0e01f8b450c6f2e7f35e
DJsudartha <djsudartha@gmail.com>
2024-07-23 21:52:17 +0800

    random bug

--- frontend/src/Features/LoginPage/ProfilePage.jsx
+++ frontend/src/Features/LoginPage/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import {React, useEffect, useState} from 'react';
 import axios from 'axios';
 import {baseURL} from '../..'
-import useParams from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 import profilePictures from '../../../public/ProfilePicture'
 import {
   MDBCol,
