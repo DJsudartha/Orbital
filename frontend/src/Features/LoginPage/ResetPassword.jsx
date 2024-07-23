cb0a97eb9d643b71da80c6586aa75acc23f403b0
DJsudartha <djsudartha@gmail.com>
2024-07-23 21:47:47 +0800

    forgot to revert index

--- frontend/src/Features/LoginPage/ResetPassword.jsx
+++ frontend/src/Features/LoginPage/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,4 @@
  * Connect to back end server for easy access to change
  */
 
-export const baseURL = "https://re-verb-server.vercel.app";
+export const baseURL = "http://localhost:1234";
